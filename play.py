43fc49aa52649dc775ec9c0d31179c2a27fd3eae
Lucas Stefano <lucassxs.sp@gmail.com>
2022-08-01 16:11:20 +0000

    minor fix'

--- play.py
+++ play.py
@@ -3,3 +3,11 @@ from kaggle_environments import make
 env = make("rps", configuration={"episodeSteps": 100}, debug=True)
 env.run(["submission.py", lambda obs, conf: random.randint(0, 2)])
 print(env.render(mode="ipython", width=600, height=600))
+
+
+####
+####rom kaggle_environments import make
+
+####env = make("rps", configuration={"episodeSteps": 100}, debug=True)
+####env.run(["submission.py", lambda obs, conf: random.randint(0, 2)])
+####print(env.render(mode="ipython", width=600, height=600))
